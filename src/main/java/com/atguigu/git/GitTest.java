60d27e9cbe27e631c949fc23439a09db679c5eb1
ouxiangyuying <17157817>
2022-03-21 22:16:33 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+/**
+ * @date 2022/3/21 0021-22:03:19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
